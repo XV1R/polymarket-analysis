517686e7c9eb6a94b0ce76e482aef92b0d83b70d
XV1R <xavytron@gmail.com>
2025-11-05 15:01:50 -0500

    Pull data down and load as df

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-import requests
 from typing import Optional, List
+import polars as pl
+import numpy as np
+import requests
 import json
+import logging
+import os
 
+
+#TODO: error handling, data structuring
 class GammaAPI():
+    '''
+    Wrapper around the polymarket gamma api endpoints 
+    '''
     def __init__(self):
         self.api_url ="https://gamma-api.polymarket.com"
+        self.logger = logging.getLogger("gamma-api")
 
     def get_events(self, limit: int = 20, 
                    offset: Optional[int] = None, 
@@ -13,12 +23,32 @@ class GammaAPI():
                    ):
 
         query = {"limit": limit}
-        return 
+        return requests.get(f"{self.api_url}/events", params=query)
+
     def get_event_by_slug(self, slug:str) -> Optional[dict]: 
         return requests.get(f"{self.api_url}/events/slug/{slug}")
+
     def get_event_by_id(): 
-        return None
+        return NotImplementedError
+
+def write_data(gamma: GammaAPI, out: str, cap: int = 10_000):
+    count = 0
+    while count < cap:
+        j = gamma.get_events(limit=500, offset=count).json()
+        with open(out, "a") as f:
+            for record in j:
+                f.write(json.dumps(record) + '\n')
+        print(f"Dumped count {count}")
+        count += 500
+
+def main():
+    gamma = GammaAPI()
+    data_path = "open-events.json"
+    if not os.path.exists(data_path):
+        write_data(gamma, data_path)
+    df: pl.DataFrame = pl.read_ndjson(data_path)
+    print(df)
 
 if __name__ == "__main__":
-    gamma = GammaAPI()
-    print(gamma.get_events().json())
+    main()
+
